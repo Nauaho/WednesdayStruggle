3d1a940aba41db88d5546ba2d1b07c63f0062765
Mateusz Grzebielec <s21560@pjwstk.edu.pl>
2024-10-22 19:08:28 +0200

    implemented model training, exported models trained. console_app base

---
+++ console_app.py
@@ -0,0 +1,6 @@
+from autogluon.tabular import TabularPredictor, TabularDataset
+
+
+predictor = TabularPredictor.load("AutogluonModels/ag-20241022_165723")
+
+input()
--- ufo-ile.py
+++ ufo-ile.py
@@ -1,6 +1,7 @@
 import kagglehub
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from autogluon.tabular import TabularPredictor, TabularDataset
 
 # -----------------------
 # Dataset:
@@ -17,13 +18,29 @@ print("Path to dataset files:", path)
 data = pd.read_csv(path)
 # print(list(data.columns))
 
-data = data.drop(columns=['city', 'state', 'comments', 'duration (hours/min)', 'date posted'])
+data = data.drop(columns=['city', 'state', 'comments', 'duration (hours/min)', 'date posted', 'shape'])
 data = data.dropna(axis='index', how='any')
+data = data.rename(columns={'longitude ': 'longitude'})
+data.loc[data["duration (seconds)"] == "2`"] = 120.0
 
 print(list(data.columns))
 print(data)
 
-y = data.pop("duration")    # Designate a value(column) for prediction and remove it from the dataset
-X_train, X_test, y_train, y_test = train_test_split(data, y, test_size=0.2, random_state=42)
+predicted_label="duration (seconds)"
+# y = data.pop(predicted_label)    # Designate a value(column) for prediction and remove it from the dataset
+data_train, data_test = train_test_split(data, test_size=0.2, random_state=42)
+
+train_data_tabular = TabularDataset(data_train)
+train_data_tabular['latitude'] = train_data_tabular['latitude'].astype("float")
+train_data_tabular['longitude'] = train_data_tabular['longitude'].astype("float")
+train_data_tabular[predicted_label] = train_data_tabular[predicted_label].astype("float")
+
+predictor = TabularPredictor(label=predicted_label, problem_type='regression').fit(train_data_tabular, presets='medium_quality')
+predictor.predict(TabularDataset(data_test))
+
+lds = predictor.leaderboard()
+print(lds)
+
+
 
 #TODO: create and train a model
