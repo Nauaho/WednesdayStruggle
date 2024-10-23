e96ef4d4a4a7cb37d8add304e0beec607a690b55
Mateusz Grzebielec <s21560@pjwstk.edu.pl>
2024-10-23 20:16:35 +0200

    reordered dataset build logic, added second edge-case handling

--- ufo-ile.py
+++ ufo-ile.py
@@ -22,18 +22,22 @@ data = data.drop(columns=['city', 'state', 'comments', 'duration (hours/min)', '
 data = data.dropna(axis='index', how='any')
 data = data.rename(columns={'longitude ': 'longitude'})
 data.loc[data["duration (seconds)"] == "2`"] = 120.0
+data.loc[data["duration (seconds)"] == "8`"] = 480.0
+
+predicted_label="duration (seconds)"
+data['latitude'] = data['latitude'].astype("float")
+data['longitude'] = data['longitude'].astype("float")
+data[predicted_label] = data[predicted_label].astype("float")
 
 print(list(data.columns))
 print(data)
 
-predicted_label="duration (seconds)"
+
 # y = data.pop(predicted_label)    # Designate a value(column) for prediction and remove it from the dataset
 data_train, data_test = train_test_split(data, test_size=0.2, random_state=42)
 
 train_data_tabular = TabularDataset(data_train)
-train_data_tabular['latitude'] = train_data_tabular['latitude'].astype("float")
-train_data_tabular['longitude'] = train_data_tabular['longitude'].astype("float")
-train_data_tabular[predicted_label] = train_data_tabular[predicted_label].astype("float")
+
 
 predictor = TabularPredictor(label=predicted_label, problem_type='regression').fit(train_data_tabular, presets='medium_quality')
 predictor.predict(TabularDataset(data_test))
