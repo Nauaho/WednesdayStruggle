710e60445915b9ecd9b9005619541f166ab57100
AleksandraDemidziuk <146822677+AleksandraDemidziuk@users.noreply.github.com>
2024-10-30 13:21:40 +0100

    Added Nazarii's changes

--- Data preparation and model creation.py
+++ Data preparation and model creation.py
@@ -15,48 +15,57 @@ def data_preparation(dataToGet):
     df[numeric_columns] = df[numeric_columns].astype('category')
     print("Does data set has any missing data?\n", df.isnull().values.any())
 
-    #Podział na oddzielne data sety
+    # Zmiana nazwy kolumn -> zrobione przez Nazariego
+    df.rename(
+        columns={
+            "1. Age": "age",
+            "2. Gender": "gender",
+            "4. Department": "department",
+            "5. Academic Year": "academic_year",
+            "7. Did you receive a waiver or scholarship at your university?": "scholarship",
+            '1. In a semester, how often you felt nervous, anxious or on edge due to academic pressure? ': 'anx_q1',
+            '2. In a semester, how often have you been unable to stop worrying about your academic affairs? ': 'anx_q2',
+            '3. In a semester, how often have you had trouble relaxing due to academic pressure? ': 'anx_q3',
+            '4. In a semester, how often have you been easily annoyed or irritated because of academic pressure?': 'anx_q4',
+            '5. In a semester, how often have you worried too much about academic affairs? ': 'anx_q5',
+            '6. In a semester, how often have you been so restless due to academic pressure that it is hard to sit still?': 'anx_q6',
+            '7. In a semester, how often have you felt afraid, as if something awful might happen?': 'anx_q7',
+            'Anxiety Label': 'anxiety_label',
+            '1. In a semester, how often have you felt upset due to something that happened in your academic affairs? ': 'str_q1',
+            '2. In a semester, how often you felt as if you were unable to control important things in your academic affairs?': 'str_q2',
+            '3. In a semester, how often you felt nervous and stressed because of academic pressure? ': 'str_q3',
+            '4. In a semester, how often you felt as if you could not cope with all the mandatory academic activities? (e.g, assignments, quiz, exams) ': 'str_q4',
+            '5. In a semester, how often you felt confident about your ability to handle your academic / university problems?': 'str_q5',
+            '6. In a semester, how often you felt as if things in your academic life is going on your way? ': 'str_q6',
+            '7. In a semester, how often are you able to control irritations in your academic / university affairs? ': 'str_q7',
+            '8. In a semester, how often you felt as if your academic performance was on top?': 'str_q8',
+            '9. In a semester, how often you got angered due to bad performance or low grades that is beyond your control? ': 'str_q9',
+            '10. In a semester, how often you felt as if academic difficulties are piling up so high that you could not overcome them? ': 'str_q10',
+            'Stress Label': 'stress_label',
+            '1. In a semester, how often have you had little interest or pleasure in doing things?': 'dep_q1',
+            '2. In a semester, how often have you been feeling down, depressed or hopeless?': 'dep_q2',
+            '3. In a semester, how often have you had trouble falling or staying asleep, or sleeping too much? ': 'dep_q3',
+            '4. In a semester, how often have you been feeling tired or having little energy? ': 'dep_q4',
+            '5. In a semester, how often have you had poor appetite or overeating? ': 'dep_q5',
+            '6. In a semester, how often have you been feeling bad about yourself - or that you are a failure or have let yourself or your family down? ': 'dep_q6',
+            '7. In a semester, how often have you been having trouble concentrating on things, such as reading the books or watching television? ': 'dep_q7',
+            "8. In a semester, how often have you moved or spoke too slowly for other people to notice? Or you've been moving a lot more than usual because you've been restless? ": 'dep_q8',
+            '9. In a semester, how often have you had thoughts that you would be better off dead, or of hurting yourself? ': 'dep_q9',
+            'Depression Label': 'depression_label'}, inplace=True)
+
+    # Podział na oddzielne data sety
     print(df.columns.tolist())
     dfAnxiety = df[[
-        '1. Age', '2. Gender', '4. Department', '5. Academic Year',
-        '7. Did you receive a waiver or scholarship at your university?',
-        '1. In a semester, how often you felt nervous, anxious or on edge due to academic pressure? ',
-        '2. In a semester, how often have you been unable to stop worrying about your academic affairs? ',
-        '3. In a semester, how often have you had trouble relaxing due to academic pressure? ',
-        '4. In a semester, how often have you been easily annoyed or irritated because of academic pressure?',
-        '5. In a semester, how often have you worried too much about academic affairs? ',
-        '6. In a semester, how often have you been so restless due to academic pressure that it is hard to sit still?',
-        '7. In a semester, how often have you felt afraid, as if something awful might happen?', 'Anxiety Label']]
+        'age', 'gender', 'department', 'academic_year', 'scholarship', 'anx_q1', 'anx_q2', 'anx_q3', 'anx_q4', 'anx_q5',
+        'anx_q6', 'anx_q7', 'anxiety_label']]
 
     dfStress = df[[
-        '1. Age', '2. Gender', '4. Department', '5. Academic Year',
-        '7. Did you receive a waiver or scholarship at your university?',
-        '1. In a semester, how often have you felt upset due to something that happened in your academic affairs? ',
-        '2. In a semester, how often you felt as if you were unable to control important things in your academic affairs?',
-        '3. In a semester, how often you felt nervous and stressed because of academic pressure? ',
-        ('4. In a semester, how often you felt as if you could not cope with all the mandatory academic activities? (e.g, '
-        'assignments, quiz, exams) '),
-        '5. In a semester, how often you felt confident about your ability to handle your academic / university problems?',
-        '6. In a semester, how often you felt as if things in your academic life is going on your way? ',
-        '7. In a semester, how often are you able to control irritations in your academic / university affairs? ',
-        '8. In a semester, how often you felt as if your academic performance was on top?',
-        '9. In a semester, how often you got angered due to bad performance or low grades that is beyond your control? ',
-        ('10. In a semester, how often you felt as if academic difficulties are piling up so high that you could not '
-        'overcome them? '), 'Stress Label']]
+        'age', 'gender', 'department', 'academic_year', 'scholarship', 'str_q1', 'str_q2', 'str_q3', 'str_q4', 'str_q5',
+        'str_q6', 'str_q7', 'str_q8', 'str_q9', 'str_q10', 'stress_label']]
 
     dfDepression = df[[
-        '1. Age', '2. Gender', '4. Department', '5. Academic Year', ('7. Did you receive a waiver or scholarship at your '
-        'university?'), '1. In a semester, how often have you had little interest or pleasure in doing things?',
-        '2. In a semester, how often have you been feeling down, depressed or hopeless?',
-        '3. In a semester, how often have you had trouble falling or staying asleep, or sleeping too much? ',
-        '4. In a semester, how often have you been feeling tired or having little energy? ',
-        '5. In a semester, how often have you had poor appetite or overeating? ',
-        ('6. In a semester, how often have you been feeling bad about yourself - or that you are a failure or have let '
-        'yourself or your family down? '), ('7. In a semester, how often have you been having trouble concentrating on '
-        'things, such as reading the books or watching television? '), ("8. In a semester, how often have you moved or spoke "
-        "too slowly for other people to notice? Or you've been moving a lot more than usual because you've been restless? ")
-        , '9. In a semester, how often have you had thoughts that you would be better off dead, or of hurting yourself? ',
-        'Depression Label']]
+        'age', 'gender', 'department', 'academic_year', 'scholarship', 'dep_q1', 'dep_q2', 'dep_q3', 'dep_q4', 'dep_q5',
+        'dep_q6', 'dep_q7', 'dep_q8', 'dep_q9', 'depression_label']]
 
     print(dfAnxiety.head())
     print(dfAnxiety.shape)
@@ -89,19 +98,19 @@ def model(predictedLabel, df, savePath):
 
 def anxiety_model():
     dfAnxiety = data_preparation("Anxiety")
-    predictedLabel = "Anxiety Label"
+    predictedLabel = "anxiety_label"
     model(predictedLabel, dfAnxiety, "AutogluonModels/Anxiety")
 
 
 def depression_model():
     dfDepression = data_preparation("Depression")
-    predictedLabel = "Depression Label"
+    predictedLabel = "depression_label"
     model(predictedLabel, dfDepression, "AutogluonModels/Depression")
 
 
 def stress_model():
     dfStress = data_preparation("Stress")
-    predictedLabel = "Stress Label"
+    predictedLabel = "stress_label"
     model(predictedLabel, dfStress, "AutogluonModels/Stress")
 
 
